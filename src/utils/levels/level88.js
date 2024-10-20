c76b60b8a331d0b3042d76088567d57c79d9c656
Sami <mohammadsamimsu@gmail.com>
2024-10-20 23:11:34 +0500

    Create level88.js

---
+++ src/utils/levels/level88.js
@@ -0,0 +1,142 @@
+
+import { altoMain, anakiwa, bittersweet, celadon, chardonnay, halfBaked, lightOrchid, lightWisteria, nomad, saharaSand } from "../colors";
+
+const level88 = {
+  size: 10,
+  colorRegions: [
+  [
+    "A",
+    "A",
+    "A",
+    "A",
+    "B",
+    "B",
+    "C",
+    "C",
+    "D",
+    "D"
+  ],
+  [
+    "E",
+    "A",
+    "A",
+    "A",
+    "B",
+    "B",
+    "C",
+    "C",
+    "D",
+    "D"
+  ],
+  [
+    "E",
+    "E",
+    "A",
+    "B",
+    "B",
+    "B",
+    "B",
+    "D",
+    "D",
+    "F"
+  ],
+  [
+    "A",
+    "E",
+    "A",
+    "B",
+    "G",
+    "G",
+    "G",
+    "D",
+    "F",
+    "F"
+  ],
+  [
+    "A",
+    "A",
+    "A",
+    "B",
+    "B",
+    "B",
+    "G",
+    "D",
+    "F",
+    "D"
+  ],
+  [
+    "H",
+    "A",
+    "A",
+    "B",
+    "B",
+    "B",
+    "B",
+    "D",
+    "D",
+    "D"
+  ],
+  [
+    "H",
+    "A",
+    "A",
+    "I",
+    "I",
+    "I",
+    "B",
+    "D",
+    "J",
+    "J"
+  ],
+  [
+    "H",
+    "A",
+    "A",
+    "B",
+    "I",
+    "B",
+    "B",
+    "D",
+    "J",
+    "D"
+  ],
+  [
+    "H",
+    "A",
+    "A",
+    "B",
+    "B",
+    "B",
+    "B",
+    "D",
+    "J",
+    "D"
+  ],
+  [
+    "A",
+    "A",
+    "A",
+    "A",
+    "B",
+    "B",
+    "B",
+    "D",
+    "D",
+    "D"
+  ]
+],
+  regionColors: {
+    A: celadon,
+    B: halfBaked,
+    C: altoMain,
+    D: anakiwa,
+    E: nomad,
+    F: lightWisteria,
+    G: saharaSand,
+    H: lightOrchid,
+    I: bittersweet,
+    J: chardonnay
+  }
+};
+
+export default level88;
