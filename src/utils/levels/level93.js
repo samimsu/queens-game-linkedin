807ef8d6f4a377dfd18f931047e44f9e9ddd70b3
Sami <mohammadsamimsu@gmail.com>
2024-10-21 01:08:58 +0500

    Create level93.js

---
+++ src/utils/levels/level93.js
@@ -0,0 +1,100 @@
+
+import { altoMain, anakiwa, bittersweet, celadon, lightOrchid, lightWisteria, malibu, saharaSand } from "../colors";
+
+const level93 = {
+  size: 8,
+  colorRegions: [
+  [
+    "A",
+    "A",
+    "A",
+    "A",
+    "A",
+    "A",
+    "A",
+    "B"
+  ],
+  [
+    "A",
+    "C",
+    "C",
+    "C",
+    "D",
+    "B",
+    "B",
+    "B"
+  ],
+  [
+    "A",
+    "C",
+    "E",
+    "E",
+    "D",
+    "F",
+    "F",
+    "F"
+  ],
+  [
+    "A",
+    "C",
+    "E",
+    "E",
+    "D",
+    "D",
+    "D",
+    "F"
+  ],
+  [
+    "A",
+    "G",
+    "E",
+    "E",
+    "E",
+    "E",
+    "H",
+    "F"
+  ],
+  [
+    "A",
+    "G",
+    "E",
+    "E",
+    "E",
+    "E",
+    "H",
+    "A"
+  ],
+  [
+    "A",
+    "G",
+    "G",
+    "G",
+    "H",
+    "H",
+    "H",
+    "A"
+  ],
+  [
+    "A",
+    "A",
+    "A",
+    "A",
+    "A",
+    "A",
+    "A",
+    "A"
+  ]
+],
+  regionColors: {
+    A: saharaSand,
+    B: bittersweet,
+    C: lightWisteria,
+    D: lightOrchid,
+    E: altoMain,
+    F: malibu,
+    G: anakiwa,
+    H: celadon
+  }
+};
+
+export default level93;
