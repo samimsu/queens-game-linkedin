1d28d0f1ad6ddd07637c9d4b4143f9dfdd4d261b
Sami <mohammadsamimsu@gmail.com>
2024-10-20 18:48:58 +0500

    Update RootLayout.jsx

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -4,7 +4,7 @@ import Footer from "../components/Footer";
 const RootLayout = ({ children }) => {
   return (
     <div className="flex flex-col h-full overflow-clip py-4">
-      <div className="flex-1 min-h-[700px] mb-3">{children}</div>
+      <div className="flex-1 min-h-[700px] mb-3 pb-16">{children}</div>
       <Footer />
     </div>
   );
