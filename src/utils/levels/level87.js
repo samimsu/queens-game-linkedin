be4d7b8707e0ff943a2b7d577de4e31be7f11454
Sami <mohammadsamimsu@gmail.com>
2024-10-20 22:43:32 +0500

    Create level87.js

---
+++ src/utils/levels/level87.js
@@ -0,0 +1,120 @@
+
+import { altoMain, anakiwa, bittersweet, celadon, chardonnay, halfBaked, lightOrchid, lightWisteria, saharaSand } from "../colors";
+
+const level87 = {
+  size: 9,
+  colorRegions: [
+  [
+    "A",
+    "A",
+    "A",
+    "A",
+    "B",
+    "B",
+    "B",
+    "B",
+    "B"
+  ],
+  [
+    "A",
+    "C",
+    "C",
+    "A",
+    "D",
+    "D",
+    "D",
+    "B",
+    "B"
+  ],
+  [
+    "A",
+    "A",
+    "A",
+    "A",
+    "D",
+    "E",
+    "D",
+    "B",
+    "B"
+  ],
+  [
+    "F",
+    "F",
+    "F",
+    "B",
+    "D",
+    "E",
+    "D",
+    "B",
+    "B"
+  ],
+  [
+    "G",
+    "G",
+    "F",
+    "B",
+    "D",
+    "D",
+    "D",
+    "B",
+    "B"
+  ],
+  [
+    "G",
+    "G",
+    "F",
+    "B",
+    "B",
+    "B",
+    "B",
+    "B",
+    "B"
+  ],
+  [
+    "F",
+    "F",
+    "F",
+    "B",
+    "B",
+    "H",
+    "H",
+    "H",
+    "H"
+  ],
+  [
+    "B",
+    "B",
+    "B",
+    "B",
+    "B",
+    "H",
+    "I",
+    "I",
+    "H"
+  ],
+  [
+    "B",
+    "B",
+    "B",
+    "B",
+    "B",
+    "H",
+    "H",
+    "H",
+    "H"
+  ]
+],
+  regionColors: {
+    A: anakiwa,
+    B: chardonnay,
+    C: lightWisteria,
+    D: celadon,
+    E: halfBaked,
+    F: altoMain,
+    G: saharaSand,
+    H: lightOrchid,
+    I: bittersweet
+  }
+};
+
+export default level87;
