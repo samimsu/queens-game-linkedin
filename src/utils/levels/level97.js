62d15e99421e6a207b92c0d069db4b30aec72441
Sami <mohammadsamimsu@gmail.com>
2024-10-21 01:24:47 +0500

    Create level97.js

---
+++ src/utils/levels/level97.js
@@ -0,0 +1,82 @@
+
+import { altoMain, anakiwa, celadon, chardonnay, halfBaked, lightOrchid, lightWisteria } from "../colors";
+
+const level97 = {
+  size: 7,
+  colorRegions: [
+  [
+    "A",
+    "A",
+    "A",
+    "A",
+    "A",
+    "A",
+    "A"
+  ],
+  [
+    "A",
+    "B",
+    "B",
+    "B",
+    "B",
+    "C",
+    "D"
+  ],
+  [
+    "A",
+    "E",
+    "F",
+    "F",
+    "F",
+    "C",
+    "D"
+  ],
+  [
+    "A",
+    "E",
+    "F",
+    "F",
+    "F",
+    "G",
+    "D"
+  ],
+  [
+    "A",
+    "E",
+    "F",
+    "F",
+    "F",
+    "G",
+    "D"
+  ],
+  [
+    "A",
+    "E",
+    "E",
+    "E",
+    "G",
+    "G",
+    "D"
+  ],
+  [
+    "A",
+    "D",
+    "D",
+    "D",
+    "D",
+    "D",
+    "D"
+  ]
+],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: lightOrchid,
+    G: halfBaked
+  }
+};
+
+export default level97;
