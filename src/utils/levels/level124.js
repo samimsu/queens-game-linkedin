c68a74bf2046dca1c5edfa5a0fe04ae719c087e1
Sami <mohammadsamimsu@gmail.com>
2024-10-24 00:10:09 +0500

    Create level124.js

---
+++ src/utils/levels/level124.js
@@ -0,0 +1,166 @@
+
+import { altoMain, anakiwa, bittersweet, celadon, chardonnay, halfBaked, lightOrchid, lightWisteria, manz, nomad, saharaSand } from "../colors";
+
+const level124 = {
+  size: 11,
+  colorRegions: [
+  [
+    "A",
+    "A",
+    "B",
+    "B",
+    "B",
+    "C",
+    "D",
+    "D",
+    "D",
+    "E",
+    "E"
+  ],
+  [
+    "A",
+    "C",
+    "B",
+    "C",
+    "C",
+    "C",
+    "D",
+    "E",
+    "E",
+    "E",
+    "E"
+  ],
+  [
+    "A",
+    "C",
+    "B",
+    "B",
+    "B",
+    "C",
+    "D",
+    "D",
+    "D",
+    "E",
+    "E"
+  ],
+  [
+    "A",
+    "C",
+    "C",
+    "C",
+    "B",
+    "C",
+    "D",
+    "E",
+    "E",
+    "E",
+    "E"
+  ],
+  [
+    "F",
+    "C",
+    "B",
+    "B",
+    "B",
+    "C",
+    "D",
+    "D",
+    "D",
+    "E",
+    "E"
+  ],
+  [
+    "F",
+    "C",
+    "C",
+    "C",
+    "C",
+    "C",
+    "C",
+    "C",
+    "C",
+    "C",
+    "E"
+  ],
+  [
+    "F",
+    "C",
+    "G",
+    "G",
+    "G",
+    "C",
+    "H",
+    "H",
+    "H",
+    "C",
+    "E"
+  ],
+  [
+    "F",
+    "F",
+    "G",
+    "I",
+    "G",
+    "J",
+    "J",
+    "H",
+    "C",
+    "C",
+    "E"
+  ],
+  [
+    "F",
+    "F",
+    "G",
+    "G",
+    "G",
+    "J",
+    "K",
+    "H",
+    "C",
+    "C",
+    "E"
+  ],
+  [
+    "F",
+    "F",
+    "G",
+    "K",
+    "K",
+    "J",
+    "K",
+    "H",
+    "C",
+    "C",
+    "C"
+  ],
+  [
+    "F",
+    "F",
+    "G",
+    "K",
+    "K",
+    "K",
+    "K",
+    "H",
+    "C",
+    "C",
+    "C"
+  ]
+],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: lightOrchid,
+    G: halfBaked,
+    H: bittersweet,
+    I: saharaSand,
+    J: nomad,
+    K: manz
+  }
+};
+
+export default level124;
