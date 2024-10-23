c2f3eb0f250f4899bb138f8d34b4f75cfe541f0d
Sami <mohammadsamimsu@gmail.com>
2024-10-23 17:12:38 +0500

    Add level 176

--- src/utils/levels/level175.js
+++ src/utils/levels/level175.js
@@ -1,109 +1,28 @@
-
-import { altoMain, anakiwa, bittersweet, celadon, chardonnay, lightOrchid, lightWisteria, nomad, saharaSand } from "../colors";
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightOrchid,
+  lightWisteria,
+  nomad,
+  saharaSand,
+} from "../colors";
 
 const level175 = {
   size: 9,
   colorRegions: [
-  [
-    "A",
-    "A",
-    "A",
-    "A",
-    "A",
-    "A",
-    "A",
-    "A",
-    "A"
+    ["A", "A", "A", "A", "A", "A", "A", "A", "A"],
+    ["A", "B", "B", "B", "B", "A", "A", "A", "A"],
+    ["A", "A", "A", "C", "C", "A", "A", "D", "A"],
+    ["A", "A", "A", "A", "C", "C", "A", "D", "A"],
+    ["A", "A", "E", "E", "F", "A", "A", "D", "A"],
+    ["A", "A", "A", "E", "F", "A", "A", "D", "A"],
+    ["A", "A", "G", "E", "F", "A", "H", "H", "A"],
+    ["I", "I", "G", "G", "F", "A", "H", "A", "A"],
+    ["I", "I", "G", "A", "A", "A", "H", "A", "A"],
   ],
-  [
-    "A",
-    "B",
-    "B",
-    "B",
-    "B",
-    "A",
-    "A",
-    "A",
-    "A"
-  ],
-  [
-    "A",
-    "A",
-    "A",
-    "C",
-    "C",
-    "A",
-    "A",
-    "D",
-    "A"
-  ],
-  [
-    "A",
-    "A",
-    "A",
-    "A",
-    "C",
-    "C",
-    "A",
-    "D",
-    "A"
-  ],
-  [
-    "A",
-    "A",
-    "E",
-    "E",
-    "F",
-    "A",
-    "A",
-    "D",
-    "A"
-  ],
-  [
-    "A",
-    "A",
-    "A",
-    "E",
-    "F",
-    "A",
-    "A",
-    "D",
-    "A"
-  ],
-  [
-    "A",
-    "A",
-    "G",
-    "E",
-    "F",
-    "A",
-    "H",
-    "H",
-    "A"
-  ],
-  [
-    "I",
-    "I",
-    "G",
-    "G",
-    "F",
-    "A",
-    "H",
-    "A",
-    "A"
-  ],
-  [
-    "I",
-    "I",
-    "G",
-    "A",
-    "A",
-    "A",
-    "H",
-    "A",
-    "A"
-  ]
-],
   regionColors: {
     A: anakiwa,
     B: chardonnay,
@@ -113,8 +32,8 @@ const level175 = {
     F: altoMain,
     G: saharaSand,
     H: nomad,
-    I: lightOrchid
-  }
+    I: lightOrchid,
+  },
 };
 
 export default level175;
