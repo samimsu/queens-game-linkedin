56396587c3091279045b8b5415681dcc2abfe515
Sami <mohammadsamimsu@gmail.com>
2024-10-23 23:13:47 +0500

    Create level114.js

---
+++ src/utils/levels/level114.js
@@ -0,0 +1,100 @@
+
+import { altoMain, anakiwa, bittersweet, celadon, chardonnay, halfBaked, lightOrchid, lightWisteria } from "../colors";
+
+const level114 = {
+  size: 8,
+  colorRegions: [
+  [
+    "A",
+    "A",
+    "A",
+    "B",
+    "B",
+    "B",
+    "C",
+    "C"
+  ],
+  [
+    "A",
+    "A",
+    "B",
+    "B",
+    "B",
+    "D",
+    "D",
+    "C"
+  ],
+  [
+    "A",
+    "B",
+    "B",
+    "B",
+    "B",
+    "D",
+    "D",
+    "B"
+  ],
+  [
+    "E",
+    "E",
+    "E",
+    "E",
+    "B",
+    "B",
+    "B",
+    "B"
+  ],
+  [
+    "F",
+    "G",
+    "G",
+    "E",
+    "E",
+    "B",
+    "B",
+    "B"
+  ],
+  [
+    "F",
+    "G",
+    "G",
+    "G",
+    "E",
+    "B",
+    "B",
+    "H"
+  ],
+  [
+    "F",
+    "F",
+    "E",
+    "E",
+    "E",
+    "B",
+    "B",
+    "H"
+  ],
+  [
+    "F",
+    "F",
+    "F",
+    "F",
+    "E",
+    "H",
+    "H",
+    "H"
+  ]
+],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: lightOrchid,
+    G: halfBaked,
+    H: bittersweet
+  }
+};
+
+export default level114;
