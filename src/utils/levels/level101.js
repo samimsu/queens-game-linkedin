c4749c2965be7fc49d2c36fdfd299bbe3a249ebf
Sami <mohammadsamimsu@gmail.com>
2024-10-23 21:48:15 +0500

    Create level101.js

---
+++ src/utils/levels/level101.js
@@ -0,0 +1,82 @@
+
+import { altoMain, anakiwa, celadon, chardonnay, halfBaked, lightOrchid, lightWisteria } from "../colors";
+
+const level101 = {
+  size: 7,
+  colorRegions: [
+  [
+    "A",
+    "A",
+    "A",
+    "A",
+    "A",
+    "A",
+    "A"
+  ],
+  [
+    "A",
+    "A",
+    "A",
+    "B",
+    "C",
+    "C",
+    "A"
+  ],
+  [
+    "A",
+    "A",
+    "B",
+    "B",
+    "B",
+    "C",
+    "C"
+  ],
+  [
+    "D",
+    "B",
+    "B",
+    "B",
+    "B",
+    "B",
+    "E"
+  ],
+  [
+    "D",
+    "D",
+    "B",
+    "B",
+    "B",
+    "E",
+    "E"
+  ],
+  [
+    "D",
+    "B",
+    "B",
+    "F",
+    "B",
+    "B",
+    "F"
+  ],
+  [
+    "G",
+    "G",
+    "G",
+    "F",
+    "F",
+    "F",
+    "F"
+  ]
+],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: lightOrchid,
+    G: halfBaked
+  }
+};
+
+export default level101;
