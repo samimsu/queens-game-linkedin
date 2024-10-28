09db02dc1c65053f3c2f08924548ed558ca5fc9a
Sami <mohammadsamimsu@gmail.com>
2024-10-28 20:28:31 +0500

    Update level181.js

--- src/utils/levels/level181.js
+++ src/utils/levels/level181.js
@@ -1,90 +1,26 @@
-
-import { altoMain, anakiwa, bittersweet, celadon, chardonnay, lightWisteria, nomad, saharaSand } from "../colors";
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  nomad,
+  saharaSand,
+} from "../colors";
 
 const level181 = {
   size: 8,
   colorRegions: [
-  [
-    "A",
-    "A",
-    "A",
-    "A",
-    "B",
-    "B",
-    "B",
-    "A"
+    ["A", "A", "A", "A", "B", "B", "B", "A"],
+    ["A", "C", "A", "A", "B", "B", "B", "A"],
+    ["D", "D", "D", "A", "B", "B", "B", "A"],
+    ["D", "D", "D", "A", "A", "A", "A", "A"],
+    ["D", "D", "D", "A", "A", "A", "E", "E"],
+    ["F", "F", "A", "A", "A", "A", "E", "E"],
+    ["F", "F", "A", "G", "G", "A", "H", "A"],
+    ["A", "A", "A", "G", "G", "A", "A", "A"],
   ],
-  [
-    "A",
-    "C",
-    "A",
-    "A",
-    "B",
-    "B",
-    "B",
-    "A"
-  ],
-  [
-    "D",
-    "D",
-    "D",
-    "A",
-    "B",
-    "B",
-    "B",
-    "A"
-  ],
-  [
-    "D",
-    "D",
-    "D",
-    "A",
-    "A",
-    "A",
-    "A",
-    "A"
-  ],
-  [
-    "D",
-    "D",
-    "D",
-    "A",
-    "A",
-    "A",
-    "E",
-    "E"
-  ],
-  [
-    "F",
-    "F",
-    "A",
-    "A",
-    "A",
-    "A",
-    "E",
-    "E"
-  ],
-  [
-    "F",
-    "F",
-    "A",
-    "G",
-    "G",
-    "A",
-    "H",
-    "A"
-  ],
-  [
-    "A",
-    "A",
-    "A",
-    "G",
-    "G",
-    "A",
-    "A",
-    "A"
-  ]
-],
   regionColors: {
     A: lightWisteria,
     B: chardonnay,
@@ -93,8 +29,8 @@ const level181 = {
     E: altoMain,
     F: bittersweet,
     G: saharaSand,
-    H: nomad
-  }
+    H: nomad,
+  },
 };
 
 export default level181;
