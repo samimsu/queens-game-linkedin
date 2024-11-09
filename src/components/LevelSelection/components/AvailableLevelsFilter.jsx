7429e1000c2b8af8be3caf79dd1674b05ef5cb9f
Sami <mohammadsamimsu@gmail.com>
2024-11-10 01:58:05 +0500

    Add help tooltip

--- src/components/LevelSelection/components/AvailableLevelsFilter.jsx
+++ src/components/LevelSelection/components/AvailableLevelsFilter.jsx
@@ -1,3 +1,4 @@
+import QuestionIcon from "@/components/icons/QuestionIcon";
 import { Switch } from "@/components/ui/switch";
 import React from "react";
 
@@ -8,6 +9,13 @@ const AvailableLevelsFilter = ({ checked, handleChange }) => {
       <label onClick={handleChange} className="pl-2">
         Available levels only
       </label>
+      <div className="relative ml-2 group">
+        <QuestionIcon />
+        <span className="absolute top-5 right-0 text-xs min-w-40 bg-white p-2 rounded-md border-2 border-slate-300 z-10 hidden group-hover:flex">
+          I've added as many levels as I could find. I'd appreciate any help in
+          finding the rest.
+        </span>
+      </div>
     </div>
   );
 };
