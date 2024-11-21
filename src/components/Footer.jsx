185ab263474f5131232e1edd4cdd8435ab65e727
Sami <mohammadsamimsu@gmail.com>
2024-11-21 21:48:29 +0500

    Add star repo button to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,30 @@
 import { Link } from "react-router-dom";
+import GithubStarButton from "./GithubStarButton";
 
 const Footer = () => {
   return (
-    <div className="mx-auto px-2 flex divide-x divide-slate-700 space-x-3">
-      <div>
-        Created by{" "}
-        <a
-          href="https://github.com/samimsu"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline font-medium"
-        >
-          Sami
-        </a>
+    <div className="px-2 space-y-3">
+      <div className="mx-auto w-fit flex items-center divide-x divide-slate-700 space-x-3">
+        <div>
+          Created by{" "}
+          <a
+            href="https://github.com/samimsu"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline font-medium"
+          >
+            Sami
+          </a>
+        </div>
+
+        <Link to="/level-builder" className="pl-3 hover:underline">
+          Level Builder
+        </Link>
       </div>
 
-      <Link to="/level-builder" className="pl-3 hover:underline">
-        Level Builder
-      </Link>
+      <div className="mx-auto w-fit">
+        <GithubStarButton variant="small" />
+      </div>
     </div>
   );
 };
