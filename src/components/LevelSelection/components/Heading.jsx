54e51f04686c06cfd2018ea2e0040717a00e20d0
Sami <mohammadsamimsu@gmail.com>
2024-12-05 14:41:40 +0500

    Replace Queen's logo in heading

--- src/components/LevelSelection/components/Heading.jsx
+++ src/components/LevelSelection/components/Heading.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-import queensLogo from "../../../assets/queens_game_logo.jpeg";
+import Queen from "@/components/Queen";
 
 const Heading = () => {
   return (
-    <div className="flex items-center space-x-3 mt-4 mb-6">
-      <img src={queensLogo} width={32} height={32} alt="Queens Logo" />
-      <h1 className="text-2xl">
-        <a
-          href="https://github.com/samimsu/queens-game-linkedin"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-700 font-medium hover:underline"
-        >
-          Queens
-        </a>
-      </h1>
-    </div>
+    <a
+      href="https://github.com/samimsu/queens-game-linkedin"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="font-medium group"
+    >
+      <div className="flex items-center space-x-3 mt-4 mb-6">
+        <Queen />
+        <h1 className="text-2xl text-blue-700 group-hover:underline">Queens</h1>
+      </div>
+    </a>
   );
 };
 
