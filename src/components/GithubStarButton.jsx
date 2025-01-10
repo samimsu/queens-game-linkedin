3ede72d46016d7213dbe30df231a654c358a1d20
Sami <mohammadsamimsu@gmail.com>
2025-01-10 15:17:04 +0500

    Change github start button

--- src/components/GithubStarButton.jsx
+++ src/components/GithubStarButton.jsx
@@ -1,27 +1,21 @@
 import React from "react";
+import GitHubButton from "react-github-btn";
 
 const GithubStarButton = ({ variant = "" }) => {
   const user = "samimsu";
   const repo = "queens-game-linkedin";
 
-  if (variant === "small") {
-    return (
-      <iframe
-        src={`https://ghbtns.com/github-btn.html?user=${user}&repo=${repo}&type=star&count=true`}
-        width="71"
-        height="20"
-        title="GitHub"
-      ></iframe>
-    );
-  }
-
   return (
-    <iframe
-      src={`https://ghbtns.com/github-btn.html?user=${user}&repo=${repo}&type=star&count=true&size=large`}
-      width="109"
-      height="30"
-      title="GitHub"
-    ></iframe>
+    <GitHubButton
+      href="https://github.com/samimsu/queens-game-linkedin"
+      data-color-scheme="no-preference: light; light: light; dark: dark;"
+      data-icon="octicon-star"
+      data-size="large"
+      data-show-count="true"
+      aria-label="Star samimsu/queens-game-linkedin on GitHub"
+    >
+      Star
+    </GitHubButton>
   );
 };
 
