7ab5ec6754ad90a87f400c0c4094147c5bab32dd
Salzie <32619234+Salzie@users.noreply.github.com>
2025-02-21 19:04:50 +0530

    Create level297.js

---
+++ src/utils/levels/level297.js
@@ -0,0 +1,120 @@
+
+import { altoMain, anakiwa, bittersweet, celadon, chardonnay, lightOrchid, lightWisteria, nomad, saharaSand } from "../colors";
+
+const level297 = {
+  size: 9,
+  colorRegions: [
+  [
+    "B",
+    "B",
+    "B",
+    "B",
+    "B",
+    "B",
+    "B",
+    "A",
+    "A"
+  ],
+  [
+    "B",
+    "B",
+    "C",
+    "B",
+    "B",
+    "I",
+    "B",
+    "B",
+    "A"
+  ],
+  [
+    "B",
+    "B",
+    "C",
+    "C",
+    "I",
+    "I",
+    "I",
+    "B",
+    "A"
+  ],
+  [
+    "B",
+    "C",
+    "C",
+    "E",
+    "I",
+    "G",
+    "B",
+    "B",
+    "A"
+  ],
+  [
+    "B",
+    "B",
+    "E",
+    "E",
+    "G",
+    "G",
+    "G",
+    "A",
+    "A"
+  ],
+  [
+    "B",
+    "E",
+    "E",
+    "F",
+    "H",
+    "H",
+    "G",
+    "A",
+    "A"
+  ],
+  [
+    "B",
+    "F",
+    "F",
+    "F",
+    "D",
+    "H",
+    "H",
+    "A",
+    "A"
+  ],
+  [
+    "B",
+    "D",
+    "F",
+    "D",
+    "D",
+    "H",
+    "D",
+    "D",
+    "A"
+  ],
+  [
+    "B",
+    "D",
+    "D",
+    "D",
+    "D",
+    "D",
+    "D",
+    "A",
+    "A"
+  ]
+],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+    H: nomad,
+    I: lightOrchid
+  }
+};
+
+export default level297;
