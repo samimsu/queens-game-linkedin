131bca92e21b59a8f1e85dcdcb46960045817c1e
Sami <mohammadsamimsu@gmail.com>
2025-02-21 19:05:25 +0500

    Prettify code and update README

--- README.md
+++ README.md
@@ -21,7 +21,7 @@
   <img src="https://github.com/user-attachments/assets/6fda1818-21f7-4d65-a288-75c9a4f30f65" width="50%" alt="queens game mockup">
 </div>
 
-## Levels Added (282/297)
+## Levels Added (283/297)
 
 - [x] 1-2
 - [ ] 3-5
--- src/utils/levels/level297.js
+++ src/utils/levels/level297.js
@@ -1,109 +1,28 @@
-
-import { altoMain, anakiwa, bittersweet, celadon, chardonnay, lightOrchid, lightWisteria, nomad, saharaSand } from "../colors";
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightOrchid,
+  lightWisteria,
+  nomad,
+  saharaSand,
+} from "../colors";
 
 const level297 = {
   size: 9,
   colorRegions: [
-  [
-    "B",
-    "B",
-    "B",
-    "B",
-    "B",
-    "B",
-    "B",
-    "A",
-    "A"
+    ["B", "B", "B", "B", "B", "B", "B", "A", "A"],
+    ["B", "B", "C", "B", "B", "I", "B", "B", "A"],
+    ["B", "B", "C", "C", "I", "I", "I", "B", "A"],
+    ["B", "C", "C", "E", "I", "G", "B", "B", "A"],
+    ["B", "B", "E", "E", "G", "G", "G", "A", "A"],
+    ["B", "E", "E", "F", "H", "H", "G", "A", "A"],
+    ["B", "F", "F", "F", "D", "H", "H", "A", "A"],
+    ["B", "D", "F", "D", "D", "H", "D", "D", "A"],
+    ["B", "D", "D", "D", "D", "D", "D", "A", "A"],
   ],
-  [
-    "B",
-    "B",
-    "C",
-    "B",
-    "B",
-    "I",
-    "B",
-    "B",
-    "A"
-  ],
-  [
-    "B",
-    "B",
-    "C",
-    "C",
-    "I",
-    "I",
-    "I",
-    "B",
-    "A"
-  ],
-  [
-    "B",
-    "C",
-    "C",
-    "E",
-    "I",
-    "G",
-    "B",
-    "B",
-    "A"
-  ],
-  [
-    "B",
-    "B",
-    "E",
-    "E",
-    "G",
-    "G",
-    "G",
-    "A",
-    "A"
-  ],
-  [
-    "B",
-    "E",
-    "E",
-    "F",
-    "H",
-    "H",
-    "G",
-    "A",
-    "A"
-  ],
-  [
-    "B",
-    "F",
-    "F",
-    "F",
-    "D",
-    "H",
-    "H",
-    "A",
-    "A"
-  ],
-  [
-    "B",
-    "D",
-    "F",
-    "D",
-    "D",
-    "H",
-    "D",
-    "D",
-    "A"
-  ],
-  [
-    "B",
-    "D",
-    "D",
-    "D",
-    "D",
-    "D",
-    "D",
-    "A",
-    "A"
-  ]
-],
   regionColors: {
     A: lightWisteria,
     B: chardonnay,
@@ -113,8 +32,8 @@ const level297 = {
     F: bittersweet,
     G: saharaSand,
     H: nomad,
-    I: lightOrchid
-  }
+    I: lightOrchid,
+  },
 };
 
 export default level297;
