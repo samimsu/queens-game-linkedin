d9c071758ed3e32779356bee2fe39ee8482e12fb
Sami <mohammadsamimsu@gmail.com>
2025-03-01 15:31:12 +0500

    Add level 305

--- src/utils/levels.js
+++ src/utils/levels.js
@@ -288,6 +288,7 @@ import level301 from "./levels/level301";
 import level302 from "./levels/level302";
 import level303 from "./levels/level303";
 import level304 from "./levels/level304";
+import level305 from "./levels/level305";
 
 export const levels = {
   level1,
@@ -580,4 +581,5 @@ export const levels = {
   level302,
   level303,
   level304,
+  level305,
 };
