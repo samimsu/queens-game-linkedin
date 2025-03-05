bec05d7b2b33264a7c978ea219f1e9836e86c914
preet717 <pritish717@outlook.com>
2025-03-05 14:28:19 +0530

    Update level309.js

--- src/utils/levels/level1.js
+++ src/utils/levels/level1.js
@@ -1,36 +1,100 @@
-import {
-  altoMain,
-  anakiwa,
-  bittersweet,
-  celadon,
-  chardonnay,
-  halfBaked,
-  lightOrchid,
-  lightWisteria,
-} from "../colors";
+
+import { altoMain, anakiwa, bittersweet, celadon, chardonnay, lightWisteria, nomad, saharaSand } from "../colors";
 
 const level1 = {
   size: 8,
   colorRegions: [
-    ["A", "A", "B", "B", "B", "C", "C", "C"],
-    ["A", "D", "B", "D", "B", "E", "C", "C"],
-    ["A", "D", "B", "D", "B", "C", "C", "C"],
-    ["A", "D", "D", "D", "B", "F", "G", "C"],
-    ["A", "D", "D", "D", "B", "F", "G", "G"],
-    ["A", "D", "H", "D", "B", "F", "G", "G"],
-    ["H", "D", "H", "D", "B", "F", "F", "G"],
-    ["H", "H", "H", "H", "G", "G", "G", "G"],
+  [
+    "A",
+    "A",
+    "B",
+    "B",
+    "B",
+    "B",
+    "C",
+    "D"
   ],
+  [
+    "A",
+    "A",
+    "A",
+    "B",
+    "B",
+    "D",
+    "C",
+    "D"
+  ],
+  [
+    "A",
+    "A",
+    "E",
+    "E",
+    "D",
+    "D",
+    "D",
+    "D"
+  ],
+  [
+    "A",
+    "F",
+    "E",
+    "D",
+    "D",
+    "G",
+    "G",
+    "G"
+  ],
+  [
+    "A",
+    "F",
+    "F",
+    "H",
+    "H",
+    "G",
+    "G",
+    "G"
+  ],
+  [
+    "A",
+    "F",
+    "H",
+    "H",
+    "H",
+    "H",
+    "G",
+    "G"
+  ],
+  [
+    "A",
+    "H",
+    "H",
+    "H",
+    "H",
+    "H",
+    "H",
+    "G"
+  ],
+  [
+    "H",
+    "H",
+    "H",
+    "H",
+    "H",
+    "H",
+    "H",
+    "H"
+  ]
+],
   regionColors: {
-    A: anakiwa,
-    B: lightWisteria,
-    C: altoMain,
-    D: bittersweet,
-    E: chardonnay,
-    F: celadon,
-    G: lightOrchid,
-    H: halfBaked,
-  },
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+    H: nomad
+  }
 };
 
 export default level1;
