d909fa22c7b2c3e2ebcb7886d74a3c368b594df4
odrail <vbmanu@gmail.com>
2025-03-06 22:22:26 +0100

    Add feature to review matches

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,59 +1,111 @@
-export const markLevelAsCompleted = (levelNumber) => {
-  let completedLevels =
-    JSON.parse(localStorage.getItem("completedLevels")) || [];
+const LOCAL_STORAGE = {
+  completedLevels: {
+    key: "completedLevels",
+    defaultValue: {}
+  },
+  clashingQueensEnabled: {
+    key: "clashingQueensEnabled",
+    defaultValue: true
+  },
+  showInstructions: {
+    key: "showInstructions",
+    defaultValue: true
+  },
+  showClock: {
+    key: "showClock",
+    defaultValue: true
+  },
+  autoPlaceXs: {
+    key: "autoPlaceXs",
+    defaultValue: false
+  },
+  groupBySize: {
+    key: "groupBySize",
+    defaultValue: false
+  },
+}
 
-  if (!completedLevels.includes(levelNumber)) {
-    completedLevels.push(levelNumber);
-    localStorage.setItem("completedLevels", JSON.stringify(completedLevels));
+const getCompletedLevels = () => {
+  try {
+    return JSON.parse(localStorage.getItem(LOCAL_STORAGE.completedLevels.key)) ?? LOCAL_STORAGE.completedLevels.defaultValue;
+  } catch (error) {
+    return LOCAL_STORAGE.completedLevels.defaultValue;
   }
+}
+
+const saveCompletedLevels = (completedLevels) => {
+  localStorage.setItem(LOCAL_STORAGE.completedLevels.key, JSON.stringify(completedLevels));
+}
+
+export const getCompletedLevel = (levelNumber) => {
+  const completedLevels = getCompletedLevels();
+  return completedLevels[levelNumber];
+}
+
+export const markLevelAsCompleted = (levelNumber, time, board) => {
+  saveLevel(levelNumber, time, board, true);
+}
+
+export const markLevelProgress = (levelNumber, time, board) => {
+  saveLevel(levelNumber, time, board, false);
+}
+
+const saveLevel = (levelNumber, time, board, completed) => {
+  const completedLevels = getCompletedLevels();
+  
+  completedLevels[levelNumber] = {
+    completed,
+    time,
+    board
+  };
+  saveCompletedLevels(completedLevels);
 };
 
 export const isLevelCompleted = (levelNumber) => {
-  const completedLevels =
-    JSON.parse(localStorage.getItem("completedLevels")) || [];
-  return completedLevels.includes(levelNumber);
+  const completedLevels = getCompletedLevels();
+  return completedLevels[levelNumber]?.completed;
 };
 
 export const resetCompletedLevels = () => {
-  localStorage.setItem("completedLevels", JSON.stringify([]));
+  saveCompletedLevels(JSON.stringify(LOCAL_STORAGE.completedLevels.defaultValue));
 };
 
 export const setClashingQueensPreference = (enabled) => {
-  localStorage.setItem("clashingQueensEnabled", JSON.stringify(enabled));
+  localStorage.setItem(LOCAL_STORAGE.clashingQueensEnabled.key, JSON.stringify(enabled));
 };
 
 export const getClashingQueensPreference = () => {
-  return JSON.parse(localStorage.getItem("clashingQueensEnabled")) ?? true; // Default to true
+  return JSON.parse(localStorage.getItem(LOCAL_STORAGE.clashingQueensEnabled.key)) ?? LOCAL_STORAGE.clashingQueensEnabled.defaultValue;
 };
 
 export const setShowInstructionsPreference = (enabled) => {
-  localStorage.setItem("showInstructions", JSON.stringify(enabled));
+  localStorage.setItem(LOCAL_STORAGE.showInstructions.key, JSON.stringify(enabled));
 };
 
 export const getShowInstructionsPreference = () => {
-  return JSON.parse(localStorage.getItem("showInstructions")) ?? true; // Default to true
+  return JSON.parse(localStorage.getItem(LOCAL_STORAGE.showInstructions.key)) ?? LOCAL_STORAGE.showInstructions.defaultValue;
 };
 
 export const setShowClockPreference = (enabled) => {
-  localStorage.setItem("showClock", JSON.stringify(enabled));
+  localStorage.setItem(LOCAL_STORAGE.showClock.key, JSON.stringify(enabled));
 };
 
 export const getShowClockPreference = () => {
-  return JSON.parse(localStorage.getItem("showClock")) ?? true; // Default to true
+  return JSON.parse(localStorage.getItem(LOCAL_STORAGE.showClock.key)) ?? LOCAL_STORAGE.showClock.defaultValue;
 };
 
 export const setAutoPlaceXsPreference = (enabled) => {
-  localStorage.setItem("autoPlaceXs", JSON.stringify(enabled));
+  localStorage.setItem(LOCAL_STORAGE.autoPlaceXs.key, JSON.stringify(enabled));
 };
 
 export const getAutoPlaceXsPreference = () => {
-  return JSON.parse(localStorage.getItem("autoPlaceXs")) ?? false; // Default to false
+  return JSON.parse(localStorage.getItem(LOCAL_STORAGE.autoPlaceXs.key)) ?? LOCAL_STORAGE.autoPlaceXs.defaultValue;
 };
 
 export const setGroupingPreference = (enabled) => {
-  localStorage.setItem("groupBySize", JSON.stringify(enabled));
+  localStorage.setItem(LOCAL_STORAGE.groupBySize.key, JSON.stringify(enabled));
 };
 
 export const getGroupingPreference = () => {
-  return JSON.parse(localStorage.getItem("groupBySize")) ?? false; // Default to false
+  return JSON.parse(localStorage.getItem(LOCAL_STORAGE.groupBySize.key)) ?? LOCAL_STORAGE.groupBySize.defaultValue;
 };
