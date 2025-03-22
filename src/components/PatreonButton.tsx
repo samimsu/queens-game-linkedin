c1c01e8d123eb4b4892efec83d97f94ad9ca8808
Sami <mohammadsamimsu@gmail.com>
2025-03-22 12:38:52 +0500

    Update PatreonButton.tsx

--- src/components/PatreonButton.tsx
+++ src/components/PatreonButton.tsx
@@ -8,10 +8,12 @@ const PatreonButton: React.FC<PatreonButtonProps> = () => {
   const { t } = useTranslation();
   return (
     <a href={patreon} target="_blank" rel="noopener noreferrer">
-      <div class="bg-[#F96C51] pt-[7px] pl-[14px] pr-[10px] pb-[10px] rounded-[30px] w-[128px] h-[35px] flex justify-between items-start">
-        <span class="w-[7px] h-[21px] bg-[#062A49]"></span>
-        <span class="w-[29px] h-[16px] bg-[white] rounded-[50%] ml-[2px] mr-[6px]"></span>
-        <span class="text-[11px] uppercase mt-[-2px] font-bold leading-[12px]">{t("SUPPORT_ME_ON_PATREON")}</span>
+      <div class="bg-[#F96C51] pt-[7px] pl-[14px] pr-[10px] pb-[10px] rounded-[30px] w-full h-[35px] flex justify-between items-start">
+        <span class="w-[4px] h-[21px] bg-[#062A49]"></span>
+        <span class="w-[16px] h-[16px] bg-[white] rounded-[50%] ml-[2px] mr-[6px]"></span>
+        <span class="text-white text-[11px] uppercase mt-[-2px] font-medium tracking-wider leading-[12px] w-[77px]">
+          {t("SUPPORT_ME_ON_PATREON")}
+        </span>
       </div>
     </a>
   );
