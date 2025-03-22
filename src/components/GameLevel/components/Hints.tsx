f4ebd4ad01a8501ddb844611fc0ce59eeadb8761
odrail <vbmanu@gmail.com>
2025-03-22 19:40:56 +0100

    add Hint component

--- src/components/GameLevel/components/Hints.tsx
+++ src/components/GameLevel/components/Hints.tsx
@@ -1,11 +1,28 @@
 import React from "react";
+import CardInfo, { CardType } from "@/components/CardInfo";
+import { useTranslation } from "react-i18next";
 
 type HintsProps = {
   message: React.ReactNode;
+  onClose?: () => void;
 };
 
-const Hints: React.FC<HintsProps> = ({ message }) => {
-  return <div>{message}</div>;
+const Hints: React.FC<HintsProps> = ({ message, onClose }) => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      {message && 
+        <CardInfo
+          title={`${t("HINT")}:`}
+          type={CardType.Closable}
+          onClose={onClose}
+        >
+          {message}
+        </CardInfo>
+      }
+    </>
+  )
 };
 
 export default Hints;
