f6f932c9f5ae873663b836b25ab956c983dfab41
odrail <vbmanu@gmail.com>
2025-03-22 20:02:14 +0100

    use default border color for CardInfo

--- src/components/CardInfo.tsx
+++ src/components/CardInfo.tsx
@@ -17,7 +17,6 @@ interface CardInfoProps extends PropsWithChildren {
 
 const CardInfo: React.FC<CardInfoProps> = ({ title = "", children, type, onClose }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
-  const { theme } = useTheme();
 
   const handleClick = () => {
     switch (type) {
@@ -33,7 +32,7 @@ const CardInfo: React.FC<CardInfoProps> = ({ title = "", children, type, onClose
   };
 
   return (
-    <div className={`rounded-[4px] p-[12px 20px 16px] pt-[12px] pl-[20px] pr-[20px] pb-[16px] mt-[12px] w-full border border-solid border-[${theme === 'light' ? 'rgba(0, 0, 0, 0.08)' : 'white'}]`}>
+    <div className={`rounded-[4px] p-[12px 20px 16px] pt-[12px] pl-[20px] pr-[20px] pb-[16px] mt-[12px] w-full border border-solid`}>
       <div className="flex justify-between">
         <h2 className="font-medium">{title}</h2>
         <button className="w-[32px] h-[32px]" onClick={handleClick}>
