b62b5f3e736bf83c8de6de4d950499199ecf7a75
Manu <76907155+odrail@users.noreply.github.com>
2025-03-24 13:39:14 +0100

    fix className for patreon button (#382)

--- src/components/PatreonButton.tsx
+++ src/components/PatreonButton.tsx
@@ -8,10 +8,10 @@ const PatreonButton: React.FC<PatreonButtonProps> = () => {
   const { t } = useTranslation();
   return (
     <a href={patreon} target="_blank" rel="noopener noreferrer">
-      <div class="bg-[#F96C51] pt-[7px] pl-[14px] pr-[10px] pb-[10px] rounded-[30px] w-full h-[35px] flex justify-between items-start">
-        <span class="w-[4px] h-[21px] bg-[#062A49]"></span>
-        <span class="w-[16px] h-[16px] bg-[white] rounded-[50%] ml-[2px] mr-[6px]"></span>
-        <span class="text-white text-[11px] uppercase mt-[-2px] font-medium tracking-wider leading-[12px] w-[77px]">
+      <div className="bg-[#F96C51] pt-[7px] pl-[14px] pr-[10px] pb-[10px] rounded-[30px] w-full h-[35px] flex justify-between items-start">
+        <span className="w-[4px] h-[21px] bg-[#062A49]"></span>
+        <span className="w-[16px] h-[16px] bg-[white] rounded-[50%] ml-[2px] mr-[6px]"></span>
+        <span className="text-white text-[11px] uppercase mt-[-2px] font-medium tracking-wider leading-[12px] w-[77px]">
           {t("SUPPORT_ME_ON_PATREON")}
         </span>
       </div>
