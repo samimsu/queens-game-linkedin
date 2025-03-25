33fed25fafd17984cb78acaa0d4d53415f3c6e36
preet717 <pritish717@outlook.com>
2025-03-25 13:24:09 +0530

    Update level329.js

--- src/utils/levels/level329.js
+++ src/utils/levels/level329.js
@@ -1,7 +1,7 @@
 
 import { altoMain, anakiwa, bittersweet, celadon, chardonnay, lightWisteria, saharaSand } from "../colors";
 
-const level1 = {
+const level329 = {
   size: 7,
   colorRegions: [
   ["A", "A", "B", "B", "B", "B", "C"],
@@ -17,4 +17,4 @@ const level1 = {
   }
 };
 
-export default level1;
+export default level329;
