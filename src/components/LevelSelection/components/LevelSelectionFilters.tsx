da70e20394a389e5080090990a1f20da8a99c877
odrail <vbmanu@gmail.com>
2025-03-30 13:40:18 +0200

    refactor level selection filters (#395)

---
+++ src/components/LevelSelection/components/LevelSelectionFilters.tsx
@@ -0,0 +1,30 @@
+// @ts-ignore
+import AvailableLevelsFilter from './AvailableLevelsFilter'
+import CompletedLevelsSwitch from './CompletedLevelsSwitch'
+
+type LevelSelectionFiltersProps = {
+  availableLevelsChecked: boolean
+  completedLevelsChecked: boolean
+  availableLevelsDisabled: boolean
+  onAvailableLevelsChange: () => void
+  onCompletedLevelsChange: () => void
+}
+
+const LevelSelectionFilters: React.FC<LevelSelectionFiltersProps> = ({ availableLevelsChecked, availableLevelsDisabled, completedLevelsChecked, onAvailableLevelsChange, onCompletedLevelsChange }) => {
+  return (
+    <div>
+      <AvailableLevelsFilter
+        checked={availableLevelsChecked}
+        handleChange={onAvailableLevelsChange}
+        disabled={availableLevelsDisabled}
+      />
+      <CompletedLevelsSwitch
+        checked={completedLevelsChecked}
+        onCheckedChange={onCompletedLevelsChange}
+        disabled={false}
+      />
+    </div>
+  );
+};
+
+export default LevelSelectionFilters;
