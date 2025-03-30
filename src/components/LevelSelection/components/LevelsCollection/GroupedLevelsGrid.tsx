03df5fd76a7c4f21919b811d17f4b6cd1d57d0c8
odrail <vbmanu@gmail.com>
2025-03-30 11:55:22 +0200

    show all levels have been completed message (#395)

--- src/components/LevelSelection/components/LevelsCollection/GroupedLevelsGrid.tsx
+++ src/components/LevelSelection/components/LevelsCollection/GroupedLevelsGrid.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { useTranslation } from "react-i18next";
 // @ts-ignore
 import { getLevelsBySize } from "@/utils/getAvailableLevels";
 // @ts-ignore
@@ -17,25 +18,33 @@ const GroupedLevelsGrid: React.FC<GroupedLevelsGridProps> = ({
   showOnlyAvailableLevels,
   hideCompletedLevels,
 }) => {
+  const { t } = useTranslation();
+  const levelsBySizeFiltered: Record<string, number[]> = useMemo(() => {
+    return Object.entries(levelsBySize)
+      .reduce((acc: Record<string, number[]>, [size, levels]) => {
+        acc[size] = levels.filter(level => filterLevel(level, orderedLevels, {
+          showOnlyAvailableLevels,
+          hideCompletedLevels,
+        }))
+        return acc
+      }, {})
+  }, [showOnlyAvailableLevels, hideCompletedLevels])
+
   return (
     <>
       {
-        Object.entries(levelsBySize).map(([size, levels]) => (
+        Object.entries(levelsBySizeFiltered).map(([size, levels]) => (
           <div key={size} className="mb-1 p-1">
             <h3 className="font-medium mb-1 text-center text-lg">
               {size}x{size}
             </h3>
+            {levels.length === 0 && <div className="text-center mb-3">{t('ALL_LEVELS_COMPLETED')}</div>}
             <div className="grid grid-cols-8 sm:grid-cols-10 gap-1">
-              {levels
-                .filter((level) =>
-                  filterLevel(level, orderedLevels, {
-                    showOnlyAvailableLevels,
-                    hideCompletedLevels,
-                  })
-                )
-                .map((level) => (
+              {
+                levels.map((level) => (
                   <LevelButton key={level} level={level} />
-                ))}
+                ))
+              }
             </div>
           </div>
         ))
--- src/components/LevelSelection/components/LevelsCollection/UngroupedLevelsGrid.tsx
+++ src/components/LevelSelection/components/LevelsCollection/UngroupedLevelsGrid.tsx
@@ -5,6 +5,7 @@ import LevelButton from "@/components/LevelSelection/components/LevelButton";
 import { getOrderedLevels } from "../../../../utils/getAvailableLevels";
 import filterLevel from "./filterLevel";
 import { LevelSelectionFilters } from "../../interfaces";
+import { useTranslation } from "react-i18next";
 
 interface UngroupedLevelsGridProps extends LevelSelectionFilters {
   className: string;
@@ -29,20 +30,23 @@ const UngroupedLevelsGrid: React.FC<UngroupedLevelsGridProps> = ({
   hideCompletedLevels,
   className,
 }) => {
+  const { t } = useTranslation();
   const ungroupedLevelsFiltered = useMemo(() => getUngroupedLevelFiltered({ showOnlyAvailableLevels, hideCompletedLevels }), [showOnlyAvailableLevels, hideCompletedLevels]);
-
   return (
-    <div
-      className={`grid grid-cols-8 sm:grid-cols-10 gap-1 p-1 text-sm ${className}`}
-    >
-      {ungroupedLevelsFiltered.map((level) => (
-        <LevelButton
-          key={level}
-          level={level}
-          disabled={!orderedLevels.includes(level)}
-        />
-      ))}
-    </div>
+    <>
+      {ungroupedLevelsFiltered.length === 0 && <div className="text-center mt-3 mb-3">{t('ALL_LEVELS_COMPLETED')}</div>}
+      <div
+        className={`grid grid-cols-8 sm:grid-cols-10 gap-1 p-1 text-sm ${className}`}
+      >
+        {ungroupedLevelsFiltered.map((level) => (
+          <LevelButton
+            key={level}
+            level={level}
+            disabled={!orderedLevels.includes(level)}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
