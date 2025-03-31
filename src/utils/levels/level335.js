1d62d83daa727e1b262324600b40d2ddaa34ec9f
Sami <mohammadsamimsu@gmail.com>
2025-03-31 12:03:28 +0500

    Add level 335

---
+++ src/utils/levels/level335.js
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand,
+} from "../colors";
+
+const level335 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "B", "B", "B", "B", "C"],
+    ["A", "A", "A", "D", "A", "A", "C"],
+    ["A", "A", "A", "D", "A", "A", "C"],
+    ["A", "E", "E", "D", "A", "A", "C"],
+    ["A", "A", "A", "A", "A", "A", "C"],
+    ["A", "F", "A", "A", "A", "A", "A"],
+    ["A", "G", "G", "G", "G", "G", "G"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+  },
+};
+
+export default level335;
