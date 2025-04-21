026bf9d354d5f457a56669e2ec3c557add89720f
Sami <mohammadsamimsu@gmail.com>
2025-04-21 20:42:55 +0500

    Update TypeScript config files

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,13 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
   },
 });
