b357e152c90df8ed8d60ded40293af27bd9dc2f9
Sami <mohammadsamimsu@gmail.com>
2025-04-22 13:38:45 +0500

    Add level 357

--- src/utils/levels.ts
+++ src/utils/levels.ts
@@ -341,6 +341,7 @@ import level353 from "./levels/level353";
 import level354 from "./levels/level354";
 import level355 from "./levels/level355";
 import level356 from "./levels/level356";
+import level357 from "./levels/level357";
 
 export const levels: { [key: string]: Level } = {
   level1,
@@ -685,4 +686,5 @@ export const levels: { [key: string]: Level } = {
   level354,
   level355,
   level356,
+  level357,
 };
