8557d229dcdde2ce6c0db808f0cb79bda44e4f6e
Sami <mohammadsamimsu@gmail.com>
2025-05-08 03:34:19 +0500

    Update HowToPlay.tsx

--- src/components/GameLevel/components/HowToPlay.tsx
+++ src/components/GameLevel/components/HowToPlay.tsx
@@ -13,20 +13,20 @@ const HowToPlay = () => {
     >
       <div className="font-medium mb-3">{t("HOW_TO_PLAY")}</div>
       <ol className="list-decimal list-inside space-y-2">
-        <li className="ml-4">
+        <li>
           <Trans i18nKey="GAME_GOAL_INSTRUCTION">
             - <b className="font-medium"> </b>
             <Queen size="20" className="inline mx-1 mb-1" />
             <b className="font-medium"></b>
           </Trans>
         </li>
-        <li className="ml-4">
+        <li>
           <Trans i18nKey="TAPS_INSTRUCTION">
             - <Queen size="20" className="inline mx-1 mb-1" />
             <Queen size="20" className="inline mx-1 mb-1" />
           </Trans>
         </li>
-        <li className="ml-4">
+        <li>
           <Trans i18nKey="TOUCH_DIAGONAL_QUEENS_INSTRUCTION">
             - <Queen size="20" className="inline mx-1 mb-1" />
           </Trans>
