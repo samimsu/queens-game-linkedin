998ed7d4428e094253bd27f3d6a648f7e2c85a8e
Sami <mohammadsamimsu@gmail.com>
2025-05-26 12:47:06 +0500

    Add May 25 bonus level

--- src/utils/bonusLevels.ts
+++ src/utils/bonusLevels.ts
@@ -1,9 +1,11 @@
 import level20250504 from "./bonus-levels/2025-05-04";
 import level20250511 from "./bonus-levels/2025-05-11";
 import level20250518 from "./bonus-levels/2025-05-18";
+import level20250525 from "./bonus-levels/2025-05-25";
 
 export const bonusLevels = {
   level20250504,
   level20250511,
   level20250518,
+  level20250525,
 };
