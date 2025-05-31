00079609620b7367c6ced4a6a64c2b36e566393c
Sami <mohammadsamimsu@gmail.com>
2025-06-01 01:25:59 +0500

    Add AuraSponge's community level

---
+++ src/utils/community-levels/level4.ts
@@ -0,0 +1,36 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand,
+} from "../colors";
+
+const level = {
+  path: "/community-level/4",
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "C", "D", "E", "E"],
+    ["A", "C", "C", "C", "D", "E", "E"],
+    ["A", "B", "B", "B", "D", "D", "E"],
+    ["A", "B", "B", "D", "D", "E", "E"],
+    ["A", "B", "B", "D", "D", "E", "E"],
+    ["A", "A", "A", "D", "F", "E", "E"],
+    ["F", "F", "F", "F", "F", "F", "G"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+  },
+  createdBy: "AuraSponge",
+  creatorLink: "https://github.com/AuraSponge",
+};
+
+export default level;
--- src/utils/communityLevels.ts
+++ src/utils/communityLevels.ts
@@ -2,9 +2,11 @@ import { CommunityLevel } from "./types";
 import level1 from "./community-levels/level1";
 import level2 from "./community-levels/level2";
 import level3 from "./community-levels/level3";
+import level4 from "./community-levels/level4";
 
 export const communityLevels: { [key: string]: CommunityLevel } = {
   level1,
   level2,
   level3,
+  level4,
 };
