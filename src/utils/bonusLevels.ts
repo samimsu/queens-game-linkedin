75a8999c6520ceaa170b319b42bb03768e488880
Sami <mohammadsamimsu@gmail.com>
2025-06-01 13:10:01 +0500

    Update bonusLevels.ts

--- src/utils/bonusLevels.ts
+++ src/utils/bonusLevels.ts
@@ -1,9 +1,9 @@
+import { BonusLevel } from "./types";
 import level20250504 from "./bonus-levels/2025-05-04";
 import level20250511 from "./bonus-levels/2025-05-11";
 import level20250518 from "./bonus-levels/2025-05-18";
 import level20250525 from "./bonus-levels/2025-05-25";
 import level20250601 from "./bonus-levels/2025-06-01";
-import { BonusLevel } from "./types";
 
 export const bonusLevels: { [key: string]: BonusLevel } = {
   level20250504,
