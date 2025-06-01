df0d979d35222e687eba0165c0b8252c2e3b81d7
Lucas <lucasf@Mac-mini-de-Lucas.local>
2025-06-01 10:47:02 +0200

    Fix issue #554 (too small Patreon button for some languages)

--- src/components/PatreonButton.tsx
+++ src/components/PatreonButton.tsx
@@ -11,7 +11,7 @@ const PatreonButton: React.FC<PatreonButtonProps> = () => {
       <div className="bg-[#F96C51] pt-[7px] pl-[14px] pr-[10px] pb-[10px] rounded-[30px] w-full h-[35px] flex justify-between items-start">
         <span className="w-[4px] h-[21px] bg-[#062A49]"></span>
         <span className="w-[16px] h-[16px] bg-[white] rounded-[50%] ml-[2px] mr-[6px]"></span>
-        <span className="text-white text-[11px] uppercase mt-[-2px] font-medium tracking-wider leading-[12px] w-[77px]">
+        <span className="text-white text-[11px] uppercase mt-[-2px] font-medium tracking-wider leading-[12px] w-[107px]">
           {t("SUPPORT_ME_ON_PATREON")}
         </span>
       </div>
