44febaccb6ba1672bd144a376ed8ae34523a2d37
trystan4861 <trystan4861@gmail.com>
2025-06-02 11:09:59 +0200

    newbadge-i18n

--- src/components/NewBadge.tsx
+++ src/components/NewBadge.tsx
@@ -1,13 +1,16 @@
+import { useTranslation } from "react-i18next";
+
 interface NewBadgeProps {
   className?: string;
 }
 
 const NewBadge = ({ className = "" }: NewBadgeProps) => {
+  const { t } = useTranslation();
   return (
     <div
       className={`rounded bg-yellow-500 text-white px-1 text-xs ${className}`}
     >
-      New
+      {t("NEW")}
     </div>
   );
 };
