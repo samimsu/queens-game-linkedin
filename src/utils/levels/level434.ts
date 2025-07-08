59ac7a5330ef116e9d73f70f856b591aef06ccca
Aditya Wikardiyan <aditya@selteer.net>
2025-07-08 17:05:18 +0800

    Create level434.ts

---
+++ src/utils/levels/level434.ts
@@ -0,0 +1,40 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightOrchid,
+  lightWisteria,
+  nomad,
+  saharaSand,
+} from "../colors";
+
+const level434 = {
+  size: 9,
+  colorRegions: [
+    ["A", "B", "B", "B", "B", "I", "I", "I", "I"],
+    ["A", "A", "B", "F", "F", "F", "I", "I", "I"],
+    ["A", "E", "E", "F", "D", "F", "G", "G", "I"],
+    ["A", "E", "H", "F", "D", "F", "G", "I", "I"],
+    ["A", "E", "E", "F", "D", "F", "G", "G", "I"],
+    ["A", "A", "E", "F", "D", "F", "C", "G", "I"],
+    ["A", "E", "E", "F", "D", "F", "G", "G", "I"],
+    ["A", "A", "A", "F", "F", "F", "A", "A", "I"],
+    ["A", "A", "A", "A", "A", "A", "A", "A", "A"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+    H: nomad,
+    I: lightOrchid,
+  },
+  isNew: true,
+};
+
+export default level434;
