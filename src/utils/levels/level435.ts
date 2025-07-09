f1a2d72c8d8e774ee56346790819613c5b1c9550
Aditya Wikardiyan <aditya@selteer.net>
2025-07-09 16:33:44 +0800

    Create level435.ts

---
+++ src/utils/levels/level435.ts
@@ -0,0 +1,37 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  nomad,
+  saharaSand,
+} from "../colors";
+
+const level435 = {
+  size: 8,
+  colorRegions: [
+    ["A", "B", "C", "C", "C", "D", "D", "D"],
+    ["A", "B", "B", "C", "D", "D", "D", "D"],
+    ["A", "B", "E", "E", "E", "E", "D", "D"],
+    ["A", "E", "E", "E", "E", "E", "E", "D"],
+    ["A", "A", "E", "F", "F", "E", "D", "D"],
+    ["A", "A", "A", "G", "G", "D", "D", "D"],
+    ["A", "A", "A", "G", "G", "D", "D", "D"],
+    ["A", "A", "H", "H", "H", "H", "D", "D"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+    H: nomad,
+  },
+  isNew: true,
+};
+
+export default level435;
