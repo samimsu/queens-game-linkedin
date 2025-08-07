224f9fa74b659b308d89bcef40128f8d758dc5a3
Sami <mohammadsamimsu@gmail.com>
2025-08-07 23:11:44 +0500

    Create levelSample.ts

---
+++ src/utils/community-levels/levelSample.ts
@@ -0,0 +1,22 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  nomad,
+  saharaSand,
+} from "../colors";
+
+const level = {
+  path: "/community-level/",
+  size: 12,
+  colorRegions: [],
+  regionColors: {},
+  solutionsCount: 0,
+  createdBy: "",
+  creatorLink: "",
+};
+
+export default level;
