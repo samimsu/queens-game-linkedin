50a18739c0a068306298827202148d05bf93e896
Sami <mohammadsamimsu@gmail.com>
2025-08-07 22:54:47 +0500

    Update links.ts

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,7 +1,7 @@
 export const patreon = "https://www.patreon.com/samimsu";
 export const portfolioSite = "https://samimsu.github.io";
 export const queensGameRepo = "https://github.com/samimsu/queens-game-linkedin";
-export const queensGameRepoNewLevelFile = `${queensGameRepo}/new/new-levels/src/utils/levels`;
+export const queensGameRepoNewLevelFile = `${queensGameRepo}/new/main/src/utils/levels`;
 export const queensGameRepoNewIssue =
   "https://github.com/samimsu/queens-game-linkedin/issues/new";
 export const officialLinkedInQueensGamePage =
