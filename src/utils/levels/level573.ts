aaf833ec551ecaf164cf98c3573509e812ac5aa3
Mark Szutor <112664262+markszutor@users.noreply.github.com>
2025-11-24 10:02:06 +0200

    Add level573
    Adding official LinkedIn Level 573 from the 24th November 2025

---
+++ src/utils/levels/level573.ts
@@ -0,0 +1,34 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand,
+} from "../colors";
+
+const level573 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "B", "C", "C", "C"],
+    ["A", "D", "A", "B", "B", "E", "E"],
+    ["A", "A", "A", "B", "B", "E", "E"],
+    ["F", "F", "A", "B", "B", "E", "E"],
+    ["G", "F", "A", "G", "B", "B", "G"],
+    ["G", "F", "G", "G", "G", "B", "G"],
+    ["G", "G", "G", "G", "G", "G", "G"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+  },
+  isNew: true,
+};
+
+export default level573;
