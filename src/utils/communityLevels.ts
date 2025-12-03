e6788066e99e94e08781d35759725ed847733c81
Sami <mohammadsamimsu@gmail.com>
2025-12-04 01:25:49 +0500

    Add community levels 361-373

--- src/utils/communityLevels.ts
+++ src/utils/communityLevels.ts
@@ -359,6 +359,19 @@ import level357 from "./community-levels/level357";
 import level358 from "./community-levels/level358";
 import level359 from "./community-levels/level359";
 import level360 from "./community-levels/level360";
+import level361 from "./community-levels/level361";
+import level362 from "./community-levels/level362";
+import level363 from "./community-levels/level363";
+import level364 from "./community-levels/level364";
+import level365 from "./community-levels/level365";
+import level366 from "./community-levels/level366";
+import level367 from "./community-levels/level367";
+import level368 from "./community-levels/level368";
+import level369 from "./community-levels/level369";
+import level370 from "./community-levels/level370";
+import level371 from "./community-levels/level371";
+import level372 from "./community-levels/level372";
+import level373 from "./community-levels/level373";
 
 export const communityLevels: { [key: string]: CommunityLevel } = {
   level1,
@@ -721,4 +734,17 @@ export const communityLevels: { [key: string]: CommunityLevel } = {
   level358,
   level359,
   level360,
+  level361,
+  level362,
+  level363,
+  level364,
+  level365,
+  level366,
+  level367,
+  level368,
+  level369,
+  level370,
+  level371,
+  level372,
+  level373,
 };
